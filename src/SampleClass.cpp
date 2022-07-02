35da44f95808f0041cb560f134cd62fc01546cbe
Ryan Day <rddays@gmail.com>
2022-07-01 21:21:44 -0500

    Update GUI to be able to put messages on screen

--- src/SampleClass.cpp
+++ src/SampleClass.cpp
@@ -1,7 +1,7 @@
 #include <cstddef>
-#include <stdio.h>
+#include <cstdio>
 #include "SampleClass.h"
-/* #include <spdlog/spdlog.h> */
+#include <spdlog/spdlog.h>
 
 #include "imgui.h"
 #include "bindings/imgui_impl_glfw.h"
@@ -11,103 +11,139 @@
 
 // Include glfw3.h after our OpenGL definitions
 #include <GLFW/glfw3.h>
-#define PI 3.14151
 
 static void glfw_error_callback(int error, const char *description)
 {
-	fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+  fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
 
 void SampleClass::do_something()
 {
-	// Setup window
-	glfwSetErrorCallback(glfw_error_callback);
-	if (!glfwInit())
-		return;
+  // Setup window
+  glfwSetErrorCallback(glfw_error_callback);
+  if (!glfwInit())
+  {
+    return;
+  }
 
-		// Decide GL+GLSL versions
+    // Decide GL+GLSL versions
 #if __APPLE__
-	// GL 3.2 + GLSL 150
-	const char *glsl_version = "#version 150";
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);		   // Required on Mac
+  // GL 3.2 + GLSL 150
+  const char *glsl_version = "#version 150";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);       // Required on Mac
 #else
-	// GL 3.0 + GLSL 130
-	const char *glsl_version = "#version 130";
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+  // GL 3.0 + GLSL 130
+  const char *glsl_version = "#version 130";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+  //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 #endif
 
-	// Create window with graphics context
-	GLFWwindow *window = glfwCreateWindow(1280, 720, "Chat boards", NULL, NULL);
-	if (window == NULL)
-		return;
-	glfwMakeContextCurrent(window);
-	glfwSwapInterval(1); // Enable vsync
+  // Create window with graphics context
+  constexpr int width = 1280;
+  constexpr int height = 720;
+  GLFWwindow *window = glfwCreateWindow(width, height, "Chat boards", nullptr, nullptr);
+  if (window == nullptr)
+  {
+    return;
+  }
 
-	bool err = glewInit() != GLEW_OK;
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1); // Enable vsync
 
-	if (err)
-	{
-		fprintf(stderr, "Failed to initialize OpenGL loader!\n");
-		return;
-	}
+  bool err = glewInit() != GLEW_OK;
 
-	int screen_width, screen_height;
-	glfwGetFramebufferSize(window, &screen_width, &screen_height);
-	glViewport(0, 0, screen_width, screen_height);
+  if (err)
+  {
+    spdlog::error("Failed to initialize OpenGL loader!");
+    return;
+  }
 
-	// Setup Dear ImGui context
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO &io = ImGui::GetIO();
-	// Setup Platform/Renderer bindings
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init(glsl_version);
-	// Setup Dear ImGui style
-	ImGui::StyleColorsDark();
+  int screen_width;
+  int screen_height;
+  glfwGetFramebufferSize(window, &screen_width, &screen_height);
+  glViewport(0, 0, screen_width, screen_height);
 
-  char buf1[200];
-  size_t bufSize = 200;
+  // Setup Dear ImGui context
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO &io = ImGui::GetIO();
+  // Setup Platform/Renderer bindings
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init(glsl_version);
+  // Setup Dear ImGui style
+  ImGui::StyleColorsDark();
 
-	while (!glfwWindowShouldClose(window))
-	{
-		glfwPollEvents();
-		glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
-		glClear(GL_COLOR_BUFFER_BIT);
+  bool initialized_at_bottom = false;
+  
+  std::vector<std::string> messages{
+    "Test: Demo message"
+  };
 
-		// feed inputs to dear imgui, start new frame
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
+  size_t bufferSize = 500;
+  char inputTextBuffer[bufferSize];
+  memset(inputTextBuffer, 0, bufferSize);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+    glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // feed inputs to dear imgui, start new frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
 
     // state = handleMessages();
     // RenderUI(state);
-		// render your GUI
-		ImGui::Begin("Chat here!!");
-    ImGui::InputTextMultiline("##text1", buf1, bufSize);
+    // render your GUI
+    ImGui::Begin("Chat here!!");
+
+    for(auto& message: messages)
+    {
+      ImGui::Text("%s", message.data()); 
+    }
+
+    if(!initialized_at_bottom)
+    {
+      ImGui::SetScrollHere(0.800f);
+      initialized_at_bottom = true;
+    }
+
+    ImGui::InputTextMultiline("##text1", inputTextBuffer, bufferSize, {300, 50});
+
+    if (ImGui::IsRootWindowOrAnyChildFocused() && !ImGui::IsAnyItemActive() && !ImGui::IsMouseClicked(0))
+       ImGui::SetKeyboardFocusHere(0);
+
+    if (ImGui::Button("Send"))
+    {
+      messages.push_back(inputTextBuffer);
+      memset(inputTextBuffer, 0, bufferSize);
+    }
+
     ImGui::End();
 
-		// Render dear imgui into screen
-		ImGui::Render();
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    // Render dear imgui into screen
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-		int display_w, display_h;
-		glfwGetFramebufferSize(window, &display_w, &display_h);
-		glViewport(0, 0, display_w, display_h);
-		glfwSwapBuffers(window);
-	}
+    int display_w, display_h;
+    glfwGetFramebufferSize(window, &display_w, &display_h);
+    glViewport(0, 0, display_w, display_h);
+    glfwSwapBuffers(window);
+  }
 
-	// Cleanup
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
+  // Cleanup
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
+  glfwDestroyWindow(window);
+  glfwTerminate();
 
 }
